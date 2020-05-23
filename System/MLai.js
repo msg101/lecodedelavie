840fe3da1eb4eb3fbb63366ea90252bae5afccdb
msg101 <dbid0100@gmail.com>
2020-05-23 12:03:22 -0700

    committ

--- System/MLai.js
+++ System/MLai.js
@@ -16,22 +16,44 @@
 class MLai{
         constructor(){
            this.requirements = "requirements";
-           
+           this.MemoryArray = [];
+           this.MemoryArraylogicsAssign = [];
         
         }
               
               
-     }
+}
+
+class Do{
+        constructor(){
+           this.requirements = "requirements";
+           this.Learnt = "blank";
+        }
+}
      
       MLai1 = new MLai();
-
+      Do1 = new Do();
       
      //MLai.prototype.attributesExample = "yes";
       
-     /*MLai.prototype.methodExample = function (){
-     //}
-      */
+     MLai.prototype.Learn = function (){
+         var i = 0;     
+         for(i=0; i<= 999999999999999;i++){
+            MLai1.MemoryArray[i] = "blank";
+            MLai1.MemoryArraylogicsAssign[i] = new Boolean(true);
+            for(j=0; j<=999999999999999;j++){
+               MLai1.MemoryArray[i][j] = "blank";
+               MLai1.MemoryArraylogicsAssign[i][j] = new Boolean(true);
+            }
 
+         }
+         MLai1.MemoryArray[0] = "People are azzholes";
+     } 
+
+    Do.prototype.changeTransformationOOP = function (){
+        Do1.Learnt = "to stay away from People";
+        
+    }
             
         
 
