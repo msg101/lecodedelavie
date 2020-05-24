48f7c84b374b74523e57bfe9062a55da3bf7007b
msg101 <dbid0100@gmail.com>
2020-05-23 17:08:05 -0700

    comitt

--- System/Consoleterminal.js
+++ System/Consoleterminal.js
@@ -1,2 +1,3 @@
 document.write("<br>" + Core1.serial);
- 
+//document.write("<br>" + MLai1.MemoryArray[0] + MLai1.MemoryArraylogicsAssign[0]);
+//document.write("<br>" + Do1.Learnt[0] + Do1.LearntlogicsAssign[0]);
