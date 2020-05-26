3b0abfb7cc46ffc46da82cdf4c311ba05401c408
msg101 <dbid0100@gmail.com>
2020-05-26 13:33:58 -0700

    committ to CVS

--- System/Consoleterminal.js
+++ System/Consoleterminal.js
@@ -1,3 +1,7 @@
-Search1.actualSearch("time");
-Search1.actualSearch("Google");
-Search1.actualSearch("People");
+//Search1.actualSearch("time");
+//Search1.actualSearch("Google");
+//Search1.actualSearch("People");
+//Search1.MainSearch("hot");
+Search1.MainSearch("nicer");
+//Search1.MainSearch("adding");
+//Search1.MainSearch("decide");
